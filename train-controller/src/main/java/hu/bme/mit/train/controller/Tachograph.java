6fcffe52d2970e79ef10007d29d5369623fcc2e7
meres <nemeth1998@gmail.com>
2020-03-02 17:25:11 +0100

    gradle modified

--- train-controller/src/main/java/hu/bme/mit/train/controller/Tachograph.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/Tachograph.java
@@ -1,4 +1,13 @@
 package hu.bme.mit.train.controller;
 
+import hu.bme.*;
+import com.google.common.collect.*;
+import java.time.LocalDateTime;
+
 public class Tachograph {
+    private Table<Integer, Integer, Integer> table = HashBasedTable.create();
+   // private TrainControllerImpl tci = new TrainControllerImpl();
+
+    Tachograph(){
+    }
 }
--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TachoTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TachoTest.java
@@ -1,4 +1,23 @@
 package hu.bme.mit.train.sensor;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import static org.mockito.Mockito.*;
+
 public class TachoTest {
+
+    int anita;
+    @Before
+    public void before() {
+        anita = 6;
+        // TODO Add initializations
+    }
+
+    @Test
+    public void ThisIsAnExampleTestStub() {
+        assert(anita == 6);
+        // TODO Delete this and add test cases based on the issues
+    }
 }
+
