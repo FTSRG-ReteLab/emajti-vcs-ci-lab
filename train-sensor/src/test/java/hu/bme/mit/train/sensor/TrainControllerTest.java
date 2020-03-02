12d69cc046d1101732a8184219156e0888857388
meres <nemeth1998@gmail.com>
2020-03-02 16:33:19 +0100

    Build bug fixed, test added

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainControllerTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainControllerTest.java
@@ -1,4 +1,19 @@
 package hu.bme.mit.train.sensor;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import static org.mockito.Mockito.*;
+
 public class TrainControllerTest {
+    public int bela;
+    @Before
+    public void before() {
+        bela = 5;
+    }
+
+    @Test
+    public void ThisIsAnExampleTestStub() {
+        assert (bela == 5);
+    }
 }
